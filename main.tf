bd05655956ef990eb48da5af7b4ca8edc70cbc6f
Wells-Huang <ayalumi@gmail.com>
2025-12-15 21:05:23 +0800

    新增 AWS 金鑰對資源並更新 EC2 實例配置

--- main.tf
+++ main.tf
@@ -2,11 +2,17 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
+resource "aws_key_pair" "deployer_key" {
+  key_name   = "ec2-key-from-id-rsa"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
 resource "aws_instance" "public_ec2" {
   ami           = "ami-0aec5ae807cea9ce0"
   instance_type = "t3.micro"
-  security_groups = ["sg-05085bae7a1b6573c"]
-  subnet_id = "subnet-0b0313ed15fb24c03" # Assuming the VPC ID provided is actually a subnet ID for the instance to be launched in
+  key_name      = aws_key_pair.deployer_key.key_name
+  security_groups = ["sg-02b2aac418b53e07e"]
+  subnet_id = "subnet-0b0313ed15fb24c03" 
   tags = {
     Name = "public ec2"
   }
